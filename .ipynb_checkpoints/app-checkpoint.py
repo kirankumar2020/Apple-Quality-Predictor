a375e2e0b73702db00abf0330259252cb0389357
kirankumar2020 <kale.kk2020@gmail.com>
2024-02-29 18:57:42 +0530

    project file

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,46 @@
+from flask import Flask,request,render_template,url_for,jsonify
+import json
+import pickle
+from utils import get_apple_quality
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/home',methods = ["GET","POST"])
+def home():
+
+    return render_template("index.html")
+
+@app.route("/apple_quality",methods=["GET", "POST"])
+def apple_quality():
+    # result  = None
+    if request.method == "POST":
+       data = request.form
+       Size = eval(data["Size"])
+       Weight = eval(data["Weight"])
+       Sweetness = eval(data["Sweetness"])
+       Crunchiness = eval(data["Crunchiness"])
+       Juiciness = eval(data["Juiciness"])
+       Ripeness = eval(data["Ripeness"])
+       Acidity = eval(data["Acidity"])
+
+       result = get_apple_quality(Size,Weight,Sweetness,Crunchiness,Juiciness,Ripeness,Acidity)
+
+       return render_template("index.html",prediction = result)
+
+    # return jsonify({"response":"successful",
+    #                 "result":f"Predicted price of mobile is : {result}"})
+    # return jsonify({"prediction": result})
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+#     @app.route("/apple_quality", methods=["GET", "POST"])
+# def apple_quality():
+#     print(request.method)  # Print the request method to console
+#     ...
---
+++ utils.py
@@ -0,0 +1,38 @@
+import pickle
+import numpy as np
+import pandas as pd
+import json
+
+
+def get_apple_quality(Size,Weight,Sweetness,Crunchiness,Juiciness,Ripeness,Acidity):
+    mobile_path = r"C:\Users\kiran\OneDrive\Desktop\PDS\01-Class-Notes\PRACTICED\Flask\Apple_Data\artifact\Apple...KNN_clf_model.pkl"
+    # mobile_path_json = r"Apple_Data\colum_data.json"
+
+    with open(mobile_path, 'rb') as f:
+        model=pickle.load(f)
+
+        with open('colum_data.json', "r") as f:
+           data = json.load(f)
+       
+    
+        test_array = np.array([Size,Weight,Sweetness,Crunchiness,Juiciness,Ripeness,Acidity],ndmin=2)
+    
+        # test_array
+
+        predicted_class = model.predict(test_array)[0]
+        print("predicted_class :",predicted_class)
+        Apple_quality = "Good" if predicted_class == 1 else "Bad"
+        print("Quality of Apple is :", Apple_quality)
+
+        return Apple_quality
+
+
+
+
+# Size = 0.05
+    # Weight = 1.68
+    # Sweetness = 1.71
+    # Crunchiness = 0.5
+    # Juiciness = 2.1
+    # Ripeness = 3.5
+    # Acidity = 1.4
